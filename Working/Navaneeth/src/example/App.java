077ffad82237cb45d6a7b736f0446c0c70f3b93c
Raz1789 <navaneeth_3@hotmail.com>
2016-12-22 20:05:16 +0400

    Few changes

--- Working/Navaneeth/src/example/App.java
+++ Working/Navaneeth/src/example/App.java
@@ -1,8 +1,8 @@
 package example;
 
-import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Insets;
 
 public class App extends ExitableJFrame {
@@ -12,28 +12,39 @@ public class App extends ExitableJFrame {
 	 */
 	private static final long serialVersionUID = 8984059550307826098L;
 
+	Image image;
 	Insets insets;
 
-	public void paint(Graphics g) {
+	public App(String filename) {
+		super(filename);
+		image = getToolkit().getImage(filename);
+	}
 
+	public void paint(Graphics g) {
 		super.paint(g);
 		if (insets == null) {
 			insets = getInsets();
 		}
-
-		g.translate(insets.left, insets.top+10);
-		System.out.println("Left: "+insets.left+"\nRight: "+ insets.right+"\n Top: "+ insets.top+"\n Bottom: "+insets.bottom);
-		g.drawString("Hello World", 1, 1);
-		g.drawChars(new char[] { 'H', 'e', 'l', 'l', 'o' }, 0, 5, 1, 15);
-		g.dispose();
+		try {
+		for (int i = 0; i < 100; i++) {
+			repaint();
+			g.drawImage(image, insets.left + i*3, insets.top, this);
+			Thread.sleep(200);
+		}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 
 	}
 
 	public static void main(String[] args) {
-		Frame f = new App();
-		f.setTitle("App");
-		f.setSize(1000, 600);
-		f.setVisible(true);
+		if (args.length > 0) {
+			Frame f = new App(args[0]);
+			f.setSize(1280, 980);
+			f.setVisible(true);
+		} else {
+			System.err.println("Please specify image to display");
+		}
 	}
 
 }
